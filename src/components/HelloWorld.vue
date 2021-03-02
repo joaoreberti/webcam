68ec54d64cfe2a9c6c46046222df786b47063619
reberti <jlreberti@ext.worten.pt>
2021-03-02 11:40:05 +0000

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div >test camera</div>
+    <div id="interactive" class="viewport">
+  <!-- QuaggaJS will populate this element -->
+    </div>
+    <button @click="testing">aqui</button>
   </div>
 </template>
 
 <script>
+import  Quagga from "quagga"
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  props: {},
+  methods: {
+    testing: async function () {
+      Quagga.init({
+    inputStream: {
+      constraints: {
+        facingMode: 'environment' // restrict camera type
+      },
+      area: { // defines rectangle of the detection
+        top: '40%',    // top offset
+        right: '0%',  // right offset
+        left: '0%',   // left offset
+        bottom: '40%'  // bottom offset
+      },
+    },
+    decoder: {
+      readers: ['ean_reader'] // restrict code types
+    },
+  },
+  (err) => {
+    if (err) {
+      this.errorMessage = `QuaggaJS could not be initialized, err: ${err}`;
+    } else {
+      Quagga.start();
+      Quagga.onDetected((res) => {
+        window.alert(`code: ${res.codeResult.code}`);
+      })
+    }
+  });
+      
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.viewport::ng-deep video {
+  width: 100%;
+  height: 240px;
+  object-fit: cover;
+}
 h3 {
   margin: 40px 0 0;
 }
